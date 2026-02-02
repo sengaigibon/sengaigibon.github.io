b085ef44804ce0eb9904d399be43ffefef7f1cb9
Javier Caballero (jknight) <jr.caba@yahoo.com>
2026-02-02 12:03:43 +0100

    Add: List of slides filenames

---
+++ lib/slideDeck.ts
@@ -0,0 +1,11 @@
+// Slide deck thumbnails
+// Add new thumbnail slides here
+export const slideThumbnails = [
+    '/images/slide-deck/thumbnail-slide1.jpg',
+    '/images/slide-deck/thumbnail-slide2.jpg',
+    '/images/slide-deck/thumbnail-slide3.jpg',
+    '/images/slide-deck/thumbnail-slide4.jpg',
+    '/images/slide-deck/thumbnail-slide5.jpg',
+    '/images/slide-deck/thumbnail-slide6.jpg',
+    '/images/slide-deck/thumbnail-slide7.jpg',
+];
